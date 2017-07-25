60d53609469e397195f88c75753df183d42354b6
zhong666 <aa900031@gmail.com>
2017-07-25 11:00:35 +0800

    使用 logger

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ module.exports = app => {
 
   app.beforeStart(function* () {
     try {
-      app.logger.info('Build start');
+      app.logger.info('[egg-nuxt] Build start');
       yield app.nuxt.build();
-      app.logger.info('Build end');
+      app.logger.info('[egg-nuxt] Build end');
     } catch (error) {
-      app.logger.error(error);
+      app.logger.error(`[egg-nuxt] ${error}`);
       process.exit(1);
     }
   });
